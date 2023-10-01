d80a4dd058ecb79d846c49624ff1a1209ab34211
Skorokhod Maxim <Skorokhodmaxim7@gmail.com>
2023-10-02 00:38:02 +0300

    header work

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { RiSearchEyeLine } from 'react-icons/ri';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="area header">
+        <div>
+          <img src="/images/logonav.png" />
+        </div>
+        <div className="search-container">
+          <form>
+            <label for="search">
+              <RiSearchEyeLine className="searchRi" />
+            </label>
+            <input
+              type="search"
+              id="search"
+              name="search"
+              placeholder="Who will we look for?"
+              className="inputStyle"
+            />
+            <button type="submit" className="buttonStyle">
+              find
+            </button>
+          </form>
+        </div>
       </header>
+      <div className="area family-photo">
+        <img src="/images/IMG_6116.jpg" alt="weddingPhoto" />
+      </div>
+      <nav className="area navbar">Navbar</nav>
+      <main className="area main">Content</main>
+      <footer className="area footer">Footer</footer>
+      <div className="area chat">Chat</div>
     </div>
   );
 }
